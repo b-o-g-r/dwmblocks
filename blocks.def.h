d56e98d8cea747909fb209709647de1fdd3b6e53
bogr <acorns.plonks.0y@icloud.com>
2022-12-21 22:50:58 +0200

    Added status bar spacings

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/	 	                            /*Update Interval*/	/*Update Signal*/
-	
+
 	{" ", "lang",	 1,		              1},
 
 	{" VOL ", "volume",     0,		              2},
