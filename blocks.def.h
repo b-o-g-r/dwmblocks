3e00a9f66a5ae664767feeef072132f17d1a50ca
bogr <acorns.plonks.0y@icloud.com>
2022-12-21 17:32:32 +0200

    Rebase

---
+++ blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/ /*Command*/	 	                            /*Update Interval*/	/*Update Signal*/
+	
+	{" RAM ", "~/Scripts/memory",	 5,		              1},
+
+	{" VOL ", "~/Scripts/volume",     0,		              2},
+
+	{" ", "~/Scripts/clock",	     60,	              0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
