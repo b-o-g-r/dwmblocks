56630d0a2a03d61bf1653fb0163f308b5dda5225
b-o-g-r <acorns.plonks.0y@icloud.com>
2023-01-18 16:52:23 +0200

    conditional battery

--- blocks.def.h
+++ blocks.def.h
@@ -1,20 +1,20 @@
 // Modify this file to change what commands output to your statusbar, and
 // recompile using the make command.
 typedef struct {
-	char* icon;
-	char* command;
-	unsigned int interval;
-	unsigned int signal;
+  char *icon;
+  char *command;
+  unsigned int interval;
+  unsigned int signal;
 } Block;
 
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {(char*)"🔸", (char*)"pgrep webui >/dev/null && echo", 1, 4},
-    {(char*)"♾", (char*)"pgrep freerdp >/dev/null && echo", 1, 5},
-    {(char*)"🌐 ",(char*)"lang", 1, 1},
-    {(char*)"🔊 ",(char*)"volume", 0, 2},
-    {(char*)"🔋 ",(char*)"battery", 1, 6},
-    {(char*)"📅 ",(char*)"clock", 60, 0},
+    {(char *)"🔸", (char *)"pgrep webui >/dev/null && echo", 1, 4},
+    {(char *)"♾", (char *)"pgrep freerdp >/dev/null && echo", 1, 5},
+    {(char *)"🌐 ", (char *)"lang", 1, 1},
+    {(char *)"🔊 ", (char *)"volume", 0, 2},
+    {(char *)"", (char *)"battery", 1, 6},
+    {(char *)"📅 ", (char *)"clock", 60, 0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
